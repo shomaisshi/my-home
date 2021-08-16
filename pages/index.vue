d8da314efeb971d9a416665a646e600a42fb79eb
shoma1219 <shoma.s.1219@gmail.com>
2021-08-16 19:55:11 +0900

    add

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <Header />
+    <Tutorial />
+    <NuxtLogo />
+  </div>
+</template>
+
+<script>
+import NuxtLogo from "../components/NuxtLogo.vue";
+export default {
+  components: { NuxtLogo },
+};
+</script>
---
+++ pages/portfolio/test.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <Header />
+    <NuxtLogo />
+  </div>
+</template>
