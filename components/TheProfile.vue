17596d8b68d68485f67d698669ce8a5b8afdcf2f
shoma1219 <shoma.s.1219@gmail.com>
2021-12-02 14:11:08 +0900

    [update]

--- components/TheProfile.vue
+++ components/TheProfile.vue
@@ -9,11 +9,11 @@
       <div class="Profile__header__right">
         <h2 class="Profile__name">shoma</h2>
         <p class="Profile__occupation">Developer / Designer</p>
-        <p class="Profile__p">
-          KLEENOTE（クレーノート）というブログを運営しています。作ったものや、好きなものを紹介しています。
-        </p>
       </div>
     </div>
+    <p>
+      KLEENOTE（クレーノート）というブログを運営しています。作ったものや、好きなものを紹介しています。
+    </p>
     <p>ご連絡はこちらのメールまでお願い致します。</p>
     <NuxtLink to="/tweet">つぶやき</NuxtLink>
   </div>
@@ -30,23 +30,23 @@
 }
 .Profile__header {
   display: flex;
+  align-items: center;
 }
 .Profile__img {
-  width: 64px;
+  width: 48px;
   border-radius: 50%;
 }
 .Profile__header__right {
   flex: 1;
+  margin-left: 8px;
 }
 .Profile__name {
   margin: 0;
 }
 .Profile__occupation {
   margin: 0;
-  margin-top: 8px;
-}
-.Profile__p {
-  /* line-height: 1.6; */
+  margin-top: 4px;
+  margin-bottom: 4px;
 }
 @media screen and (min-width: 640px) {
   .Profile {
